f8b074d22beaaced17fe348052d0d16a03b7c5b9
fazza7777 <aungbarlaay7777@gmail.com>
2021-06-29 02:12:33 +0630

    categories seeding process

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 let express = require('express'),
         app = express();
 let seeder = require('./database/seeder')
+
 seeder.seedCat()
-.then(res=>console.log(res))
-.catch(err=>console.log(err))
-// ********** Seeder Cat **********
+// .then(res=>console.log(res))
+// .catch(err=>console.log(err))
 
 app.listen(process.env.PORT,()=>console.log(`server is running at port ${process.env.PORT}`))
--- database/seeder.js
+++ database/seeder.js
@@ -1,16 +1,30 @@
 const fs = require('fs');
-const { resolve } = require('path');
-let cat = require('./cat');
+let Cats = require('./cat');
 
 // first read seed json file
 
 let seedCat = ()=>{
-    return new Promise((resolve,reject)=>{
+
         fs.readFile('categories.json',(err,data)=>{
-           if(err) reject(err)
-           resolve(data);
+           if(err){
+              console.log('File cannot be read ',err);
+            }else{
+                let cats = JSON.parse(data); // change buffer string to json data
+                cats.forEach((cat)=>{
+                    // create new obj because json file not include since field 
+                    let obj = {
+                        'id' : cat.id,
+                        'name' : cat.name,
+                        'image' : cat.image,
+                        'since' : Date.now()
+                    }
+                    Cats.save(obj)
+                    .then(res=>console.log(res))
+                    .catch(err=>console.log(err))
+                });
+           }
         });
-    });
+
 }
 
 module.exports = {
