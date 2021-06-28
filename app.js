2b109d1cafd0978bb1057ad7200bfc6abfec0196
fazza7777 <aungbarlaay7777@gmail.com>
2021-06-29 00:59:47 +0630

    Category Save or Create Category

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 require('dotenv').config();
 let express = require('express'),
         app = express();
+const cat = require('./database/cat');
 
+let catObj = {
+    id : 1,
+    name : 'Computer',
+    image : 'acer.png',
+    since :  Date.now()
+}
+ cat.save(catObj)  // cat.js promise return
+.then(res=>console.log(res))
+.catch(error=> console.log(error))
 
 app.listen(process.env.PORT,()=>console.log(`server is running at port ${process.env.PORT}`))
--- database/cat.js
+++ database/cat.js
@@ -1,3 +1,30 @@
 let db = require('./db');
 
 let Cat = db.Cat;
+
+// **** get cat *******
+let all = ()=>{
+    return new Promise((resolve, reject)=>{
+        Cat.find({},(err,data)=>{
+            if(err) reject(err)
+            resolve(data);
+        });
+    });
+
+}
+// **** save cat *******
+let save = (obj)=>{
+    return new Promise((resolve,reject)=>{
+        let cat = new Cat(obj);
+        cat.save((err,data)=>{
+            if(err) reject(err)
+            resolve(data)
+        })
+    });
+}
+
+
+module.exports = {
+    all,
+    save
+}
