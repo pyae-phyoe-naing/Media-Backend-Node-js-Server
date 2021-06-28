69db23a5a2c9608dc818e8122def38926622dd71
fazza7777 <aungbarlaay7777@gmail.com>
2021-06-29 02:57:45 +0630

    setup product chema using mongoose-auto-increment for id auto increment

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
 let express = require('express'),
         app = express();
-let seeder = require('./database/seeder')
 
-seeder.seedCat()
-// .then(res=>console.log(res))
-// .catch(err=>console.log(err))
 
 app.listen(process.env.PORT,()=>console.log(`server is running at port ${process.env.PORT}`))
--- database/db.js
+++ database/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 let url = 'mongodb://localhost:27017/mediaDB';
 const connect = mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true,useFindAndModify: false,useCreateIndex: true},);
+const autoIncrement = require('mongoose-auto-increment');
+//autoIncrement.initialize(mongoose.createConnection(url));
+// or
+autoIncrement.initialize(mongoose.connection);
 
 let Schema = mongoose.Schema;
 
+// ************ Category Schema *********** //
 let CatScheme = new Schema({
     id : {type:Number,required:true},
     name : {type:String,required:true},
@@ -13,6 +18,20 @@ let CatScheme = new Schema({
 
 let Cat = mongoose.model('category',CatScheme); // create table
 
+// ************ Product Schema *********** //
+let ProductScheme = new Schema({
+    cat_id : {type:Number,required:true},
+      name : {type:String,required:true},
+     price : {type:Number,required:true},
+     image : {type:String,required:true},
+     description : {type:String,required:true},
+     since : {type:Date,required:true},
+});
+// make autoincrement use mongoose-auto-increatement
+ProductScheme.plugin(autoIncrement.plugin,'product'); // product => table name
+let Product = mongoose.model('product',ProductScheme); // create products table
+
 module.exports = {
-    Cat
+    Cat,
+    Product
 }
