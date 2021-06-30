e2b5948c82966b01ac559ade75ab87bb09697236
fazza7777 <aungbarlaay7777@gmail.com>
2021-06-30 23:08:10 +0630

    use passport middleware

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ require('dotenv').config();
 let express = require('express'),
         app = express()
         jwt = require('jsonwebtoken')
+        JwtStrategy = require('passport-jwt').Strategy,
+        ExtractJwt = require('passport-jwt').ExtractJwt,
         passport = require('passport'),
         bodyParser = require('body-parser');
+
+let User = require('./database/user');
+let jwtOption = {};
+jwtOption.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOption.secretOrKey = process.env.SECRET;     
+let myStrategy = new JwtStrategy(jwtOption, function(payload, done) {
+       let email = payload.email;
+       let name = payload.name;
+       User.findUserByEmail(email)
+       .then((user)=>{
+               if(user.name == name){
+                 done(null,user);
+               }
+       })
+       .catch(err=>done(null,err))
+    });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
+passport.use(myStrategy);
 
 let guestRoute = require('./routes/guest') (express);
 let userRoute = require('./routes/user')(express,jwt);
--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,8 @@ module.exports = (express,passport) => {
       router.get('/home',(req,res)=>{
         res.send('Admin Home Route');
       });
+      router.get('/users',passport.authenticate('jwt', { session: false }),(req,res)=>{
+           res.send('Admin secret route')
+      });
      return router;
 }
